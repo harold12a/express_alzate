ddedf89b928ab6ea447db9d165826f535446f989
harold alzate <haroldalzate99@gmail.com>
2023-07-19 08:09:29 -0500

    modified:   router/mangas.js

--- router/mangas.js
+++ router/mangas.js
@@ -4,11 +4,13 @@ import create from "../controllers/mangas/create.js";
 import validator from "../middlewares/validator.js";
 import schema_create from "../schemas/mangas/create.js";
 import passport from "../middlewares/passport.js";
+import read_one from "../controllers/mangas/read_one.js";
 
 let mangasRouter = Router()
 
 mangasRouter.post('/', passport.authenticate('jwt', { session: false }),create)
 mangasRouter.post('/',passport.authenticate('jwt', { session: false }), validator(schema_create), create)
 mangasRouter.get('/',passport.authenticate('jwt', { session: false }), read)
+
 
 export default mangasRouter
