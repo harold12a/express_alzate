392f736f8b8d40be018e3b8e167428412938b8ef
santiagogutierre8 <santiagogutierre8@gmail.com>
2023-07-20 18:53:11 -0500

    se sube actividad m09-endpoint

--- controllers/authors/read.js
+++ controllers/authors/read.js
@@ -2,16 +2,40 @@ import Author from "../../models/Author.js";
 
 export default async (req,res, next)=>{
         try {
-            let all = await Author.find()
-            if(all){
+            let consultas = {};
+            let pagination = {page:1, limit:4};
+            if(req.query.page){
+                pagination.page = req.query.page
+            }
+
+            if(req.query.name){
+                consultas.name =  new RegExp(req.query.name,'i')
+            }
+            if(req.query.user_id){
+                consultas.user_id = req.query.user_id.split(',')
+            }
+            let skip = (pagination.page > 0) ?
+                (pagination.page - 1) * pagination.limit:0
+            let limit = pagination.limit >0 ?
+                pagination.limit : 0
+
+            let all = await Author.find(consultas, 'name last_name city country createdAt photo').skip(skip).limit(limit).sort({name:1}).populate('user_id', 'name')
+            if(all.length>0){
+                let total = await Author.countDocuments(consultas)
+                let pages = Math.ceil(total / pagination.limit)
+                
+                let prev_pages = pagination.page === 1? null : Number(pagination.page) -1
+                let next_page = pagination.page === pages ? null : Number (pagination.page) +1
+                
                 return res.status(200).json({
                     response: all,
-                    message:' authors founds!'
+                    message:' authors founds!',
+                    prev_pages,
+                    next_page,
                 })
             }else{
                 return res.status(404).json({
                     response: null,
-
                     message:'  authors NOT founds!'
 
                 });
--- router/mangas.js
+++ router/mangas.js
@@ -3,11 +3,15 @@ import read from "../controllers/mangas/read.js";
 import create from "../controllers/mangas/create.js";
 import validator from "../middlewares/validator.js";
 import schema_create from "../schemas/mangas/create.js";
+import passport from "passport";
+import has_permition from "../middlewares/has_permition.js";
+import read_news from "../controllers/mangas/read_news.js";
 
 let mangasRouter = Router()
 
-mangasRouter.post('/',create)
-mangasRouter.post('/', validator(schema_create), create)
-mangasRouter.get('/', read)
+mangasRouter.post('/',create);
+mangasRouter.post('/', validator(schema_create), create);
+mangasRouter.get('/', read);
+mangasRouter.get('/news', passport.authenticate("jwt",{"session":false}), has_permition, read_news)
 
 export default mangasRouter
