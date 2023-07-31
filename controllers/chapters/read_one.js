95b669e75165ed20baca99fa25dbb52007bd0cb6
andreaprada1993 <andrea_prada_93@hotmail.com>
2023-07-31 17:08:38 -0500

    se realiza cambios pequeños es read_one

--- controllers/chapters/read_one.js
+++ controllers/chapters/read_one.js
@@ -3,7 +3,7 @@ import Chapter from '../../models/Chapter.js';
 export default async (req, res, next) => {
     try {
         let id = req.params.id;
-        Chapter.findById(id, "title cover_photo pages -_id")
+        let one = await Chapter.findById(id, "title cover_photo pages -_id")
         let allChapters = await Chapter.findOne({ _id: { $gt: id } });
 
         return res.status(200).json({
