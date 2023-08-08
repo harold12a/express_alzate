588b6339f0c122f45cc78ed08356136bec2d7aa5
andreaprada1993 <andrea_prada_93@hotmail.com>
2023-08-07 19:50:10 -0500

    se agrega los cambios al back para obtener el manga id

--- controllers/chapters/read_one.js
+++ controllers/chapters/read_one.js
@@ -3,13 +3,16 @@ import Chapter from '../../models/Chapter.js';
 export default async (req, res, next) => {
     try {
         let id = req.params.id;
-        let one = await Chapter.findById(id, "title cover_photo pages -_id")
-        let allChapters = await Chapter.findOne({ _id: { $gt: id } });
+        let chapter = await Chapter.findById(id, "title cover_photo pages order manga_id -_id")
+        let next = await Chapter.findOne({ _id: { $gt: id } }, "_id");
 
         return res.status(200).json({
             success: true,
-            response: one,
-            next: allChapters ? allChapters._id : null
+            response: {
+                chapter,
+                next: next ? next._id :null,
+            },
+            message: "Found",
         })
     } catch (error) {
         next(error);
