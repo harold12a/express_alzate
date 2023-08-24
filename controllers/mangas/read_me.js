7c52da0eb3d3e42b7f54737d2184ac2a70b0f12a
harold alzate <haroldalzate99@gmail.com>
2023-08-24 18:34:56 -0500

    modified:   controllers/mangas/read_me.js 	modified:   middlewares/error.handler.js

--- controllers/mangas/read_me.js
+++ controllers/mangas/read_me.js
@@ -1,27 +1,30 @@
 import Manga from "../../models/Manga.js";
 
-export default async function read_me (req, res, next){
+export default async function read_me(req, res, next) {
 
     try {
-        let all = await Manga.find({author_id: req.body.author_id})
-        if(all.length > 0){
-            return res.status(200).json({
-                response: all,
-                success: true,
-                message: 'Mangas Found'
-            })
-        }else{
-            return res.status(404).json({
-                success: false,
-                response: null,
-                message: 'Mangas not found'
-
-            })
+        let all = await Manga.find({ author_id: req.body.author_id }).populate('category_id').sort({ title: 1 })
+        let mangaByCategory = {}
+        for (let each of all) {
+            // each es cada uno de los mangas del array all
+            // each tiene todas las prpiedades del manga tengoque componer un objeto con las listas ordenadas por categoria 
+            // let manga = {shonen: lista de mangas shonen de este author , shojo: lista de mangas shojo  de este author ...}
+            if (mangaByCategory[each.category_id.name]) {
+                mangaByCategory[each.category_id.name].push(each)
+            } else {
+                mangaByCategory[each.category_id.name] = [each]
+            }
         }
+        return res.status(200).json({
+            response: mangaByCategory,
+            success: true,
+            message: 'Mangas Found'
+        })
+
 
     } catch (error) {
         next(error);
-        
+
     }
 
 }
--- middlewares/error.handler.js
+++ middlewares/error.handler.js
@@ -1,5 +1,5 @@
 const error_handler = (error,req,res,next)=>{
-    console.log(error);
+    console.log(error.message);
     return res.status(500).json({
         success:false,
         response: null,  
