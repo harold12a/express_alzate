b06f06954d0dcf797f6ecf3f88c4bc6b542d24c5
Santiago Gutierrez Serna <125933900+SantiagoGutierrez896@users.noreply.github.com>
2023-08-28 10:38:40 -0500

    se suben cambios en update 1, 2, controller author y companies (#40)

--- controllers/auth/update_role_1.js
+++ controllers/auth/update_role_1.js
@@ -3,27 +3,25 @@ import Author from "../../models/Author.js";
 
 export default async(req,res,next)=>{
     try {
-        let one = await User.findByIdAndUpdate(req.params.id,{
+        let one = await User.findByIdAndUpdate(req.params.id, {
             role:1
         },{
             new:true
         })
-        let author = await Author.findOneAndUpdate({user_id:req.params.id},{
-            active: true
-        },{
-            new:true
-        })
+        let author = await Author.findOne({user_id:req.params.id})
+        author.active= !author.active
+        await author.save()
         if(one){return res.status(200).json({
             success: true,
-            response: one,
+            response : author,
             message:"Role change"
         })
     }else{
-        return res.status(400).json({
+        return res.status(404).json({
             success: false,
             response:null,
             message:"not found"
-    })  
+        })
     }
     } catch (error) {
         next(error)
